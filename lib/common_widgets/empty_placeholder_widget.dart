365d0a87a51a16a0535d8af8ef391ed9d92ef907
ulrichsinn <uli@addtothenoise.com>
2025-05-05 09:59:23 -0700

    utiliywidgets added to ‚common widgets‘

---
+++ lib/common_widgets/empty_placeholder_widget.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_metronome/common_widgets/primary_button.dart';
+import 'package:go_router/go_router.dart';
+
+import '../constants/app_sizes.dart';
+import '../routing/app_router.dart';
+
+
+/// Placeholder widget showing a message and CTA to go back to the home screen.
+class EmptyPlaceholderWidget extends StatelessWidget {
+  const EmptyPlaceholderWidget({super.key, required this.message});
+  final String message;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(Sizes.p16),
+      child: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              message,
+              style: Theme.of(context).textTheme.headlineMedium,
+              textAlign: TextAlign.center,
+            ),
+            gapH32,
+            PrimaryButton(
+              onPressed: () => context.goNamed(AppRoute.metronome.name),
+              text: 'Go Home',
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
